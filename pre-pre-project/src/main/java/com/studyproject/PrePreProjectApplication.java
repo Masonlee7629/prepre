b56b86d1271d13d7dce55fabbc0d1f6f30d85c5a
mason <masonlee7629@gmail.com>
2022-11-20 03:08:13 +0900

    dd

---
+++ pre-pre-project/src/main/java/com/studyproject/PrePreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.studyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrePreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrePreProjectApplication.class, args);
+	}
+
+}
